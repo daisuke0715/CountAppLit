a431f21d0e71714214c1304aac5dfe0fa56a132b
daisuke0715 <da1i9su9ke7.0715@icloud.com>
2021-08-15 00:55:00 +0900

    initial-commit

--- Count/ViewController.swift
+++ Count/ViewController.swift
@@ -8,12 +8,46 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var number: Int = 0
+    @IBOutlet weak var label: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func plus() {
+        number += 1
+        label.text = String(number)
+        checkNum()
+    }
+    
+    @IBAction func minus() {
+        number -= 1
+        label.text = String(number)
+        checkNum()
+    }
+    
+    @IBAction func multi() {
+        number *= 2
+        label.text = String(number)
+        checkNum()
+    }
+    
+    @IBAction func division() {
+        number /= 2
+        label.text = String(number)
+        checkNum()
     }
 
+    func checkNum() {
+        if number >= 10 {
+            label.textColor = UIColor.red
+        } else {
+            label.textColor = UIColor.blue
+        }
+    }
+    
 
 }
 
